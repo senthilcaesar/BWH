db024144bd521495515d25caf36227d1b5eaae38
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2022-11-21 13:47:35 -0500

    luna help

---
+++ luna/sp_output.sh
@@ -0,0 +1,7 @@
+echo "Spindle found in N2"
+echo "-----------------------------------------------------"
+destrat outN2.db +SPINDLES -r F CH -v FFT AMP DUR DENS -p 3
+echo "\n"
+echo "Spindle found in N2 and N3"
+echo "-----------------------------------------------------"
+destrat outN2N3.db +SPINDLES -r F CH -v FFT AMP DUR DENS -p 3
