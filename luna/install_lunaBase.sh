cd97509c4b04d316368e077a252cbfdb0f5d73da
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2022-12-02 14:40:10 -0500

    luna updates

--- luna/install_lunaBase.sh
+++ luna/install_lunaBase.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-install_lunaBase=0
-install_lunaR=0
-fftw_dir='/opt/homebrew/Cellar/fftw/3.3.10_1'
-lgbm_dir='/Users/sq566/Downloads/tmp/LightGBM'
+
+install_lunaBase=1
+install_lunaR=1
+fftw_dir=$HOME/Programme/fftw-3.3.8
+lgbm_dir=$HOME/Programme/LightGBM
+module load R/3.6.3
 
 # Install base luna
 if [ ${install_lunaBase} -eq 1 ]
@@ -10,21 +12,23 @@ then
     cd $HOME
     git clone https://github.com/remnrem/luna-base.git
     cd luna-base
-    make ARCH=MAC FFTW=${fftw_dir} -j4 LGBM=1 LGBM_PATH=${lgbm_dir}
+    make -j4 FFTW=${fftw_dir} LGBM=1 LGBM_PATH=${lgbm_dir}
     echo alias luna=$HOME/luna-base/luna >> $HOME/.bash_profile
-    echo alias luna=$HOME/luna-base/destrat >> $HOME/.bash_profile
-    echo alias luna=$HOME/luna-base/behead >> $HOME/.bash_profile
+    echo alias destrat=$HOME/luna-base/destrat >> $HOME/.bash_profile
+    echo alias behead=$HOME/luna-base/behead >> $HOME/.bash_profile
+    echo alias fixrows=$HOME/luna-base/fixrows >> $HOME/.bash_profile
     source ~/.bash_profile
 else
     echo "Skipping luna-base installation"
 fi
 
 # Install luna R
+lib_install_loc=$HOME/R/x86_64-pc-linux-gnu-library/3.6
 if [ ${install_lunaR} -eq 1 ]
 then
     cd $HOME
     git clone https://github.com/remnrem/luna.git
-    FFTW=${fftw_dir} R CMD INSTALL luna
+    FFTW=${fftw_dir} R CMD INSTALL -l ${lib_install_loc} luna
 else
     echo "Skipping luna-R installation"
 fi
