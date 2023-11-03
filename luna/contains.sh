4589be6f698b439f1074540a64f481101552fc1b
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2023-11-03 11:14:40 -0400

    luna CONTAINS command

--- luna/contains.sh
+++ luna/contains.sh
@@ -1,33 +1,13 @@
 #!/bin/bash
 
-arrText=($(cat sub.txt))
-
+arrText=($(cat tmp/sub.txt))
+rm -rf both.txt
 
 for i in "${!arrText[@]}"; do
-
     idx=$(( $i + 1 ))
-
-    val=`luna s.lst $idx silent=T -s 'CONTAINS sig=${airflow}' | grep -i PRESENT | wc -l`
-    
+    val=`luna sl/xall.lst $idx silent=T -s 'CONTAINS sig=C3,C4' | grep -i PRESENT | awk '{sum+=$6;} END{print sum;}'`
+    echo ${arrText[$idx]}   $val
     if [ "$val" -eq "2" ]; then
-        echo ${arrText[$i]} >> 2.txt
-    
-    elif [ "$val" -eq "3" ]; then
-        echo ${arrText[$i]} >> 3.txt
-
-    else
-        echo ${arrText[$i]} >> none.txt
-
+        echo ${arrText[$i]} >> bothChannelPresent.txt
     fi
-
-    #luna s.lst ${i} silent=T -s 'CONTAINS sig=${airflow}'
-
-    #HAS_EEG=$?
-
-    #if [[ ${HAS_EEG} -eq 0 ]]; then
-    #    echo "Airflow channel present"
-    #else
-    #    echo "No found..."
-    #fi
-
 done
