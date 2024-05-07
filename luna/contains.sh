966ad7d17bb90147fbe50b960827edc4e609e30f
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2024-05-07 13:01:06 -0400

    updates contains

--- luna/contains.sh
+++ luna/contains.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-arrText=($(cat tmp/sub.txt))
-rm -rf both.txt
+arrText=($(cat tmp/chin_already_referenced.txt))
 
 for i in "${!arrText[@]}"; do
     idx=$(( $i + 1 ))
-    val=`luna sl/xall.lst $idx silent=T -s 'CONTAINS sig=C3,C4' | grep -i PRESENT | awk '{sum+=$6;} END{print sum;}'`
+    val=`luna sl/chin_already_referenced.lst $idx silent=T -s 'CONTAINS sig=LChin_RChin,LChin_CChin,RChin_CChin' | grep "PRESENT" | awk '{sum+=$6;} END{print sum;}'`
     echo ${arrText[$idx]}   $val
-    if [ "$val" -eq "2" ]; then
-        echo ${arrText[$i]} >> bothChannelPresent.txt
+    if [ "$val" -eq 3 ]; then
+        echo ${arrText[$i]} >> threePresent.txt
+
+    elif [ "$val" -eq 2 ]; then
+        echo ${arrText[$i]} >> twoPresent.txt
+
+    elif [ "$val" -eq 1 ]; then
+        echo ${arrText[$i]} >> onePresent.txt
     fi
 done
