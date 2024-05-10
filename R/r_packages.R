1419088bcb8574aa248514aaa92c9596c6d07fc0
Senthil Palanivelu <spalanivelu@mgh.harvard.edu>
2024-05-10 10:55:54 -0400

    Install R Packages

---
+++ R/r_packages.R
@@ -0,0 +1,2 @@
+install.packages(c("shiny", "git2r", "data.table", "plotrix", "geosphere", "DT", "shinyFiles", 
+                   "shinydashboard", "lubridate", "wkb", "aws.s3", "shinybusy", "shinyjs", "shinythemes"))
